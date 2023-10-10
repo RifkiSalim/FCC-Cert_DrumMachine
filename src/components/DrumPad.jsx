93d1ba55d56bde8172d5e8752a9e28f944ce190d
Rifki Salim <contact@rifkisalim.com>
2023-10-10 13:18:08 +0530

    🦄 refactor(DrumPad): code cleanup

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,26 +1,37 @@
+// React Imports
 import React, { useRef, useState } from "react";
+
+// DrumPad Component
 const DrumPad = ({ id, audioSrc, triggerKey, onPlay, onDone }) => {
+  // States
   const [isActive, setIsActive] = useState(false);
+
+  // Refs
   const audioRef = useRef(null);
+
+  // Handlers
   const handleClick = () => {
     if (audioRef.current) {
+      // call onPlay event
       onPlay();
       setIsActive(true);
       // Pause the audio if it's currently playing
       audioRef.current.pause();
-      // Set the currentTime to 0 to reset the audio to the beginning
+      // Reset audio time to the beginning
       audioRef.current.currentTime = 0;
       // Then play the audio
       audioRef.current
         .play()
         .then(() => {})
         .catch((error) => {
-          // Handle any errors
+          // Pause and reset on error
           audioRef.current.pause();
           audioRef.current.currentTime = 0;
         })
         .finally(() => {
+          // Call onDone
           onDone();
+          // isActive back to false after .5s delay
           const timeoutId = setTimeout(() => {
             setIsActive(false);
           }, 500);
@@ -31,22 +42,25 @@ const DrumPad = ({ id, audioSrc, triggerKey, onPlay, onDone }) => {
 
   return (
     <div className="col">
+      {/* DrumPad clickable button  */}
       <button
         id={id}
         className={`drum-pad container-fluid btn btn-square-md d-flex flex-column align-items-center justify-content-center ${
           isActive ? "btn-secondary" : "btn-primary"
         } `}
         style={{
-          height: 100,
+          height: 100, // 100px height
         }}
         onClick={handleClick}
       >
+        {/* Audio src */}
         <audio
           ref={audioRef}
           className="clip"
           id={triggerKey}
           src={audioSrc}
         ></audio>
+        {/* Label */}
         <span>{triggerKey}</span>
       </button>
     </div>
