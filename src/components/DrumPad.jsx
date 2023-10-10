b744a756f8e159803ddcdfcbceb8211a3190aede
Rifki Salim <contact@rifkisalim.com>
2023-10-10 12:59:10 +0530

    ✨ feat(DrumPad): implement DrumPad component

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+const DrumPad = ({ id, audioSrc, triggerKey, onPlay, onDone }) => {
+  const [isActive, setIsActive] = useState(false);
+  const audioRef = useRef(null);
+  const handleClick = () => {
+    if (audioRef.current) {
+      onPlay();
+      setIsActive(true);
+      // Pause the audio if it's currently playing
+      audioRef.current.pause();
+      // Set the currentTime to 0 to reset the audio to the beginning
+      audioRef.current.currentTime = 0;
+      // Then play the audio
+      audioRef.current
+        .play()
+        .then(() => {})
+        .catch((error) => {
+          // Handle any errors
+          audioRef.current.pause();
+          audioRef.current.currentTime = 0;
+        })
+        .finally(() => {
+          onDone();
+          const timeoutId = setTimeout(() => {
+            setIsActive(false);
+          }, 500);
+          return () => clearTimeout(timeoutId);
+        });
+    }
+  };
 
-export default function DrumPad() {
   return (
-    <div className="col drum-pad">
-      <div
-        className="container-fluid btn btn-primary btn-square-md bg-primary d-flex flex-column align-items-center justify-content-center"
+    <div className="col">
+      <button
+        id={id}
+        className={`drum-pad container-fluid btn btn-square-md d-flex flex-column align-items-center justify-content-center ${
+          isActive ? "btn-secondary" : "btn-primary"
+        } `}
         style={{
           height: 100,
         }}
+        onClick={handleClick}
       >
-        <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
-        <span>Q</span>
-      </div>
+        <audio
+          ref={audioRef}
+          className="clip"
+          id={triggerKey}
+          src={audioSrc}
+        ></audio>
+        <span>{triggerKey}</span>
+      </button>
     </div>
   );
-}
+};
+
+export default DrumPad;
